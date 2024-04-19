60ec8907a68892e024cf1b5a985841f9d547478a
Joel Marpaung <joelmarpaung.mj@gmail.com>
2024-04-19 20:10:35 +0700

    adding the registration user for firebase authentication

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'register.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
